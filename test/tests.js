50c55b4c305b29092ce788c9d675e709605c6ed7
marcelodebittencourt <tchelopoa@gmail.com>
2021-09-27 15:36:10 -0300

    Add mocha usage to enable ci integration

---
+++ test/tests.js
@@ -0,0 +1,30 @@
+//const fullBaseURL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=<<appid>>';
+require('dotenv/config');
+const axios = require('axios');
+const url = require('url');
+
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Get OpenWeatherMap API testing', async function () {
+    it('should weather data, 200 HTTP status code, city London and country GB', async function () {
+        const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
+        const city = 'London';
+        const countryCode = 'uk';
+        const appid = process.env.APP_ID;
+
+        let payload = { q: `${city},${countryCode}`, appid: `${appid}` };
+
+        const params = new url.URLSearchParams(payload);
+
+        let res = await axios.get(`${baseURL}?${params}`);
+
+        let data = res.data;
+        console.log(data);
+        assert.equal(200, res.status);
+        assert.equal(city, res.data.name);
+        assert.equal('GB', res.data.sys.country);
+    })
+});
+
+//makeGetRequest();
